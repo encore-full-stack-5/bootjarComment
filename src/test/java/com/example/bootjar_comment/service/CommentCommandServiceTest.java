3bb044f6edea5da7eb3d1d612cf5812b180df7ce
박미람 <myurm@a-dev.co.kr>
2024-06-23 11:10:25 +0900

    [Test] comment CRUD Test 코드

---
+++ src/test/java/com/example/bootjar_comment/service/CommentCommandServiceTest.java
@@ -0,0 +1,94 @@
+package com.example.bootjar_comment.service;
+
+import com.example.bootjar_comment.command.CommentCommand;
+import com.example.bootjar_comment.dto.CreateCommentDto;
+import com.example.bootjar_comment.dto.UpdateCommentDto;
+import com.example.bootjar_comment.repository.CommandRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.cglib.core.Local;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CommentCommandServiceTest {
+
+    @InjectMocks
+    private CommentCommandService commentCommandService;
+    @InjectMocks
+    private CommentQueryService commentQueryService;
+    @Mock
+    CommandRepository commandRepository;
+
+    @Test
+    void 댓글_조회 () {
+        // given
+        CommentCommand createComment = new CommentCommand(1L, 1L, 1L, "Test comment", LocalDateTime.now(), LocalDateTime.now());
+        when(commandRepository.findByTodoId(1L)).thenReturn(Flux.just(createComment));
+        // when
+        Flux<CommentCommand> commentFlux = commentQueryService.getCommentList(1L);
+        // then
+        StepVerifier.create(commentFlux)
+                .expectNextMatches(comment -> comment.getContent().equals("Test comment"))
+                .verifyComplete();
+    }
+
+    @Test
+    void 댓글_생성 () {
+        // given
+        CreateCommentDto commentDto = new CreateCommentDto(1L, 1L, "Test comment", LocalDateTime.now(), LocalDateTime.now());
+        when(commandRepository.save(any(CommentCommand.class))).thenReturn(Mono.empty());
+        // when
+        Mono<Void> comment = commentCommandService.createComment(1L, commentDto).then();
+        // then
+        StepVerifier.create(comment)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void 댓글_수정 () {
+        // given
+        UpdateCommentDto commentDto = new UpdateCommentDto("Test comment Update");
+        CommentCommand existingComment = CommentCommand.builder()
+                .id(1L)
+                .userId(1L)
+                .todoId(1L)
+                .content("Initial content")
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+        when(commandRepository.findById(1L)).thenReturn(Mono.just(existingComment));
+        when(commandRepository.save(any(CommentCommand.class))).thenAnswer(invocation -> {
+            CommentCommand updatedComment = invocation.getArgument(0);
+            return Mono.just(updatedComment);
+        });
+        // when
+        Mono<Void> updateComment = commentCommandService.updateComment(1L, commentDto.getContent());
+        // then
+        StepVerifier.create(updateComment)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void 댓글_삭제 () {
+        // given
+        CommentCommand comment = new CommentCommand(1L, 1L, 1L, "Test comment", LocalDateTime.now(), LocalDateTime.now());
+        when(commandRepository.deleteById(1L)).thenReturn(Mono.empty());
+        // when
+        Mono<Void> deleteComment = commentCommandService.deleteComment(1L);
+        // then
+        StepVerifier.create(deleteComment)
+                .verifyComplete();
+    }
+}
