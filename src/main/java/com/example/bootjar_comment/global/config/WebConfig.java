e1440176c75b62cb08c0ab15e78e44afe448163e
ParkMiram <myurm@naver.com>
2024-06-30 14:32:14 +0900

    [Refactor] CORS 수정

--- src/main/java/com/example/bootjar_comment/global/config/WebConfig.java
+++ src/main/java/com/example/bootjar_comment/global/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebFluxConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://34.121.86.244/*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedOrigins("http://34.121.86.244")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
